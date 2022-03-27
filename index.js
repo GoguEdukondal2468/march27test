ba7fc99826636dea530804ea2afb5e3e495369be
GOGU EDUKONDAL <97788201+GoguEdukondal2468@users.noreply.github.com>
2022-03-27 16:15:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var library=[
+    {
+        author:'Bill Gates',
+        title:'The Road Ahead',
+        readingStatus:true
+    },
+    {
+        author:'Steve Jobs',
+        title:'Water Isaacson',
+        readingStatus:true
+    },
+    {
+        author:'Suzanne Collins',
+        title:'Mockingjay:The Final Book of The Hunger Games',
+        readingStatus:false
+    }
+
+
+];
+console.log(library)
